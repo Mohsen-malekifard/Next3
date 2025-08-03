21ee5942ad8fabeb0281789964d26a343d5b0169
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-03 18:20:52 +0000

    A

---
+++ my-app/src/app/lib/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id : "1",
+        title : "T-Shirt",
+        price : "10000"
+        context : "This is a T-shirt Originally"
+    },
+    {
+        id : "2",
+        title : "Shirt",
+        price : "250000"
+        context : "This is a Shirt Originally"
+    },
+    {
+        id : "3",
+        title : "Skarf",
+        price : "333000"
+        context : "This is a Skarf Originally"
+    }
+]
+
+export default data;
